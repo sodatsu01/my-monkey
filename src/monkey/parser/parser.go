4adb607ee905b07122e9aa1961b0e2488209320f
sodatsu01 <darumasanga.korondaa@gmail.com>
2021-10-01 12:18:02 +0900

    2.6.7 整数リテラル式の解析

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -5,6 +5,7 @@ import(
   "monkey/lexer"
   "monkey/token"
   "fmt"
+	"strconv"
 )
 
 type (
@@ -30,6 +31,7 @@ func New(l *lexer.Lexer) *Parser {
   }
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
   p.nextToken()
   p.nextToken()
@@ -162,4 +164,16 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 	}
 	leftExp := prefix()
 	return leftExp
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
