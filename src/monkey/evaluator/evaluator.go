61e507f2796b300a377ec7e7f0ccacb543b76765
sodatsu01 <darumasanga.korondaa@gmail.com>
2021-10-06 10:32:37 +0900

    3.5.3 boolean

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -5,6 +5,10 @@ import (
 	"monkey/object"
 )
 
+var (
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -13,6 +17,8 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -25,3 +31,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 	return result
 }
+
+func nativeBoolToBooleanObject(input bool) {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
+
