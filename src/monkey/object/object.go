147e1b6e6eca469b132a9c0b9cde986bb43603a2
sodatsu01 <darumasanga.korondaa@gmail.com>
2021-10-07 14:17:31 +0900

    4.3 len関数

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -17,6 +17,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
 	FUNCTION_OBJ = "FUNCTION"
+  BUILTIN_OBJ = "BUILTIN"
 )
 
 type Object interface {
@@ -110,3 +111,12 @@ type String struct {
 }
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string { return s.Value }
+
+type BuitinFunction func(args ...Object) Object
+
+type Builtin struct {
+  Fn BuitinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string { return "builtin function" }
