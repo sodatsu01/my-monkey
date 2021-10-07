b69f038444fd853127ab2741ff99cf41735f2fe0
sodatsu01 <darumasanga.korondaa@gmail.com>
2021-10-07 12:21:09 +0900

    4.2.1 String字句解析

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -39,6 +39,9 @@ func (l *Lexer) NextToken() token.Token {
 		} else {
 			tok = newToken(token.ASSIGN, l.ch)
 		}
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '-':
@@ -132,4 +135,15 @@ func (l *Lexer) readNumber() string {
 
 func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer)readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
+	}
+	return l.input[position:l.position]
 }
--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -7,23 +7,27 @@ import (
 
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
-	let ten = 10;
-	let add = fn(x, y) {
-		x + y;
-	};
-	
-	let result = add(five, ten);
-	!-/*5;
-	5 < 10 > 5;
-	if (5 < 10) {
-		return true;
-	} else {
-		return false;
-	}
+let ten = 10;
 
-	10 == 10;
-	10 != 9;
-	`
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+10 == 10;
+10 != 9;
+"foobar"
+"foo bar"
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -102,6 +106,8 @@ func TestNextToken(t *testing.T) {
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 
